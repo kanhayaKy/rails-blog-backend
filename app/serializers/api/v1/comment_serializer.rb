fe679dd593653376dcb0da9f86e259803a611a9d
kanhaya kumar yadav <kanhayaky120@gmail.com>
2023-05-06 14:47:54 +0530

    updated comment and post serializer

--- app/serializers/api/v1/comment_serializer.rb
+++ app/serializers/api/v1/comment_serializer.rb
@@ -2,7 +2,10 @@ class Api::V1::CommentSerializer < ActiveModel::Serializer
   attributes :id, :comment, :created_at, :updated_at, :author, :post
 
   def author
-    object.user.username
+    {
+      username: object.user.username,
+      name: "#{object.user.first_name} #{object.user.last_name}"
+    }
   end
 
   def post
--- app/serializers/api/v1/post_serializer.rb
+++ app/serializers/api/v1/post_serializer.rb
@@ -2,7 +2,10 @@ class Api::V1::PostSerializer < ActiveModel::Serializer
   attributes :id, :description, :likes, :created_at, :updated_at, :author
 
   def author
-    object.user.slice(:username, :first_name, :last_name)
+    {
+      username: object.user.username,
+      name: "#{object.user.first_name} #{object.user.last_name}"
+    }
   end
 
   has_many :comments
